8cb36d9b0a91970be30aa8c81a675b5bfc2d212e
CodeMaestro1000 <timothyakoji@gmail.com>
2022-06-10 19:25:43 +0100

    Minor Bugfix to add option for skipping perspective transformation

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ from transform import get_top_down_view
 from shape_detector import detect_shapes
 from form_generator import generate_output_html
 
-image_path = 'Test Images/test_img_7.jpg'
+image_path = 'Test Images/generated_shapes.png'
 output_filename = "test_output.html"
 image = cv2.imread(image_path)
 
-warped_image = get_top_down_view(image)
-data = detect_shapes(warped_image)
+skip_top_down_process = True
+
+if not skip_top_down_process:
+    warped_image = get_top_down_view(image)
+    data = detect_shapes(warped_image)
+else:
+    try:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    except:
+        pass
+    data = detect_shapes(image)
+
 generate_output_html(data, output_filename)
