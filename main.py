6e2a9d026ce14335d33167dce35e5a2ac781b50f
CodeMaestro1000 <timothyakoji@gmail.com>
2022-06-17 09:24:00 +0100

    Code refactoring to use argument parser

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
-import cv2
+import cv2, argparse
 from functions import get_top_down_view, detect_shapes, generate_output_html
 
-image_path = 'Test Images/test_img_7.jpg'
-output_filename = "test_output.html"
+parser =  argparse.ArgumentParser()
+parser.add_argument("-i", "--image", required=True, help='Filename for input image')
+parser.add_argument("-o", "--output_filename", required=True, help='Filename for output HTML')
+parser.add_argument("-t", "--skip_perspective_transform", default=False, help='Skip processing of cropping region of interest')
+parser.add_argument("-s", "--show_output", default=True, help="Show intermediate outputs")
+
+args = vars(parser.parse_args())
+
+image_path = args['image']
+output_filename = args['output_filename']
 image = cv2.imread(image_path)
 
-skip_top_down_process = False # set to true if you already have a cropped image of your form
-# change to False if you don't want intermediate outputs but it is highly recommended that you leave it as True 
-# because you might need to tweak some values for performing edge detection
-show_output = True 
+skip_perspective_transform = args['skip_perspective_transform'] # set to true if you already have a cropped image of your form use False if you don't want intermediate outputs
+# but it is highly recommended that you leave it as True  because you might need to tweak some values for performing edge detection
+show_output = args['show_output']
 
-if not skip_top_down_process:
+if skip_perspective_transform:
+    data = detect_shapes(image, duplicate_threshold=2, delta=5, show_output=show_output)
+else:
     warped_image = get_top_down_view(image, show_output=show_output)
     data = detect_shapes(warped_image, duplicate_threshold=2, delta=5, show_output=show_output) if warped_image is not None else None
-else:
-    data = detect_shapes(image, duplicate_threshold=2, delta=5, show_output=show_output)
+    
 if data:
     generate_output_html(data, output_filename)
 # if no shape is found, no output file will be created
